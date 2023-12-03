4839b5c7da7c3bccddce999c585335719970a760
Jeet Barot <jeetbarot1998@gmail.com>
2023-12-03 19:15:53 +0530

    Lect7_LogicalOperators

--- Chap4/Lect7_LogicalOperators.java
+++ Chap4/Lect7_LogicalOperators.java
@@ -1,4 +1,39 @@
 package Chap4;
 
 public class Lect7_LogicalOperators {
+    public static void main(String[] args) {
+//      Due to double "||" operator, if 1st condition is true, 2nd condition will not be checked.
+        int a1 = 5;
+        int b1 = 15;
+        if(++a1 <10 || ++b1>15){
+            a1++;
+        }
+        else{
+            b1++;
+        }
+        System.out.println(a1 + ", " + b1);
+        System.out.println(a1 + b1);
+
+//      Due to double "||" operator, if 1st condition is false, then only 2nd condition will be checked.
+        a1 = 10;
+        b1 = 15;
+        if(++a1 <10 || ++b1>15){
+            a1++;
+        }
+        else{
+            b1++;
+        }
+        System.out.println(a1 + ", " + b1);
+        System.out.println(a1 + b1);
+
+        for(int i = 0; true|false; i++){
+            if(--i==0 || i==2){
+                System.out.println("YES " + i);
+                break;
+            }
+            else if(i++ %2 ==0){
+                System.out.println("NO " + i);
+            }
+        }
+    }
 }
