3bc3b41ba5367de0f32552055db4e2d17b22af74
Jeet Barot <jeetbarot1998@gmail.com>
2024-04-05 18:27:31 +0530

    Concurrency_2_3

--- OCP/Concurrency_2/Concurrency_2_3.java
+++ OCP/Concurrency_2/Concurrency_2_3.java
@@ -1,4 +1,4 @@
-package OCP.Concurrency;
+package OCP.Concurrency_2;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -8,26 +8,24 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
-class UserAccount{
+class UserBankAccount{
     private int balance = 10000;
     private void deposit(int amt){balance += amt;}
     private void withdraw(int amt){balance -= amt;}
     int getBalance(){return balance;}
-    public static synchronized void tx(UserAccount acc1, UserAccount acc2,int amt){
+    public static synchronized void tx(UserBankAccount acc1, UserBankAccount acc2, int amt){
         acc1.withdraw(amt);
         acc2.deposit(amt);
     }
 
-    public static void tx2(UserAccount acc1, UserAccount acc2,int amt){
+    public static void tx2(UserBankAccount acc1, UserBankAccount acc2, int amt){
         acc1.withdraw(amt);
         acc2.deposit(amt);
     }
 }
-
-public class Concurrency_4 {
-    private UserAccount acc1 = new UserAccount();
-    private UserAccount acc2 = new UserAccount();
+public class Concurrency_2_3 {
+    private UserBankAccount acc1 = new UserBankAccount();
+    private UserBankAccount acc2 = new UserBankAccount();
     private Lock lock1 = new ReentrantLock();
     private Lock lock2 = new ReentrantLock();
 
@@ -61,7 +59,7 @@ public class Concurrency_4 {
 //            lock2.lock();
             acquireLock();
             try {
-                UserAccount.tx(acc1,acc2, r.nextInt(100));
+                UserBankAccount.tx(acc1,acc2, r.nextInt(100));
             }
             finally {
                 lock1.unlock();
@@ -78,7 +76,7 @@ public class Concurrency_4 {
 //            lock1.lock();
             acquireLock();
             try {
-                UserAccount.tx(acc2,acc1, r.nextInt(100));
+                UserBankAccount.tx(acc2,acc1, r.nextInt(100));
             }
             finally {
                 lock1.unlock();
@@ -94,22 +92,23 @@ public class Concurrency_4 {
     }
 }
 
+
 class MyConcurrentThreadC41{
     public static void main(String[] args) throws InterruptedException {
-        Concurrency_4 runner = new Concurrency_4();
+        Concurrency_2_3 runner = new Concurrency_2_3();
         Thread t1 = new Thread(() ->{
-           try {
-               runner.firstThread();
-           }
-           catch (Exception e){
-           }
+            try {
+                runner.firstThread();
+            }
+            catch (Exception e){
+            }
         });
         Thread t2 = new Thread(() ->{
-           try {
-               runner.secondThread();
-           }
-           catch (Exception e){
-           }
+            try {
+                runner.secondThread();
+            }
+            catch (Exception e){
+            }
         });
 
         t1.start();
@@ -120,8 +119,8 @@ class MyConcurrentThreadC41{
     }
 }
 
-class ConcurrentHashMapExample{
-    static ConcurrentHashMap <Integer, String> chm = new ConcurrentHashMap<>();
+class ConcurrentHashMapEx{
+    static ConcurrentHashMap<Integer, String> chm = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         chm.put(1, "M");
@@ -133,7 +132,7 @@ class ConcurrentHashMapExample{
     }
 }
 
-class CMODemoThread extends Thread{
+class CMEDemoThread extends Thread{
     static List<String> al = new ArrayList<>();
 
     @Override
@@ -150,7 +149,7 @@ class CMODemoThread extends Thread{
         al.add("A");
         al.add("B");
         al.add("C");
-        CMODemoThread cme = new CMODemoThread();
+        CMEDemoThread cme = new CMEDemoThread();
         cme.start();
         al.forEach(each_str -> {
             System.out.println("Main thread " + each_str);
@@ -206,6 +205,10 @@ class ArrayListConcurrentEx{
                 "When you make an iterator object internally jvm\n" +
                 "thinks that it will be iterated upon now hence you \n" +
                 "are not supposed to change it as it will give cme");
+
+
+
+        
         ArrayList <Integer> al = new ArrayList<>();
         al.add(2);
         al.add(4);
@@ -221,3 +224,57 @@ class ArrayListConcurrentEx{
 }
 
 
+
+class PrinterResource {
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void printDocument(String document, String threadName) throws InterruptedException {
+        lock.lockInterruptibly(); // Attempt to acquire the lock, can be interrupted
+        try {
+            System.out.println(threadName + " is printing: " + document);
+            Thread.sleep(2000); // Simulating printing time
+        } finally {
+            lock.unlock(); // Release the lock
+            System.out.println(threadName + " finished printing.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PrinterResource printer = new PrinterResource();
+
+        Thread urgentPrintTask = new Thread(() -> {
+            try {
+                printer.printDocument("URGENT Document", "UrgentTaskThread");
+            } catch (InterruptedException e) {
+                System.out.println("Urgent task interrupted while waiting for the printer.");
+            }
+        });
+
+        Thread normalPrintTask = new Thread(() -> {
+            try {
+                printer.printDocument("Normal Document", "NormalTaskThread");
+            } catch (InterruptedException e) {
+                System.out.println("Normal task interrupted while waiting for the printer.");
+            }
+        });
+
+        urgentPrintTask.start();
+
+        // Simulating a delay before starting the normal print task
+        try {
+            Thread.sleep(1000); // Delay of 1 second
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        normalPrintTask.start();
+
+        // Interrupting the urgent print task after a delay
+        try {
+            Thread.sleep(500); // Delay of 0.5 seconds
+            urgentPrintTask.interrupt(); // Interrupt the urgent print task
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
